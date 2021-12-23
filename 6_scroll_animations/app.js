0c9b3fa33463e23c50cfd4fe851c9d4d1d3acf3c
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-12-23 12:44:21 +0000

    day 6 scroll animation

---
+++ 6_scroll_animations/app.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll('.box');
+
+
+window.addEventListener('scroll', boxCheck);
+
+boxCheck();
+
+function boxCheck(){
+ const triggerBottom = window.innerHeight * 0.8;
+
+ boxes.forEach((box)=>{
+   const boxTop = box.getBoundingClientRect().top;
+   
+   boxTop < triggerBottom ? box.classList.add('show') : box.classList.remove('show');
+ })
+
+};
