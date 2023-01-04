8e73f500cbb5f3ac882fdfeccb9023712b5abfb9
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2023-01-04 22:37:22 +0000

    counters project

--- 13_choice_picker/app.js
+++ 13_choice_picker/app.js
@@ -1,67 +1,69 @@
-const tagArea = document.getElementById('tags');
-const textArea = document.getElementById('textarea');
+const tagArea = document.getElementById("tags");
+const textArea = document.getElementById("textarea");
 
 textArea.focus();
 
-textArea.addEventListener('keyup', (e)=>{
+textArea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
-  
-  if(e.key.toLowerCase() === 'enter'){
-    setTimeout(()=>{
+
+  if (e.key.toLowerCase() === "enter") {
+    setTimeout(() => {
       e.target.value = "";
-    },20)
-   
+    }, 20);
+
     randomization();
   }
-})
+});
 
-function createTags(input){
-  const tags = input.split(',').filter(tag => tag.trim() !== "").map((tag) => tag.trim());
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
 
-  tagArea.innerHTML = '';
+  tagArea.innerHTML = "";
 
   tags.forEach((tag) => {
-    const newTag = document.createElement('span');
-    newTag.classList.add('tag');
+    const newTag = document.createElement("span");
+    newTag.classList.add("tag");
     newTag.innerText = tag;
     tagArea.appendChild(newTag);
     // tagArea.innerHTML += `<span class="tag">${tag}</span>` //using template literals
   });
 }
 
-
-function randomization(){
+function randomization() {
   textArea.disabled = true;
   const times = 30;
 
-  const interval = setInterval(()=>{
+  const interval = setInterval(() => {
     const randomTag = pickTag();
     highlightTag(randomTag);
-    setTimeout(()=>{
+    setTimeout(() => {
       unHighlightTag(randomTag);
-    }, 100)
+    }, 100);
   }, 100);
 
-  setTimeout(()=>{
+  setTimeout(() => {
     clearInterval(interval);
-    
-    setTimeout(()=>{
+
+    setTimeout(() => {
       const randomTag = pickTag();
       highlightTag(randomTag);
-      textArea.disabled = false
-    }, 100)
-  }, times*100);
+      textArea.disabled = false;
+    }, 100);
+  }, times * 100);
 }
 
-function pickTag(){
-  const tags = document.querySelectorAll('.tag');
-  return tags[Math.floor(Math.random()*tags.length)];
+function pickTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
 }
 
-function highlightTag(tag){
-  tag.classList.add('highlight');
+function highlightTag(tag) {
+  tag.classList.add("highlight");
 }
 
-function unHighlightTag(tag){
-  tag.classList.remove('highlight');
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
